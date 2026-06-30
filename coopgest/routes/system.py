3bf4e7c38081aa78d06a460cf895e7adfb6de001
Nuno <nunom@email.com>
2026-06-30 14:40:57 +0100

    Make healthcheck public

--- coopgest/routes/system.py
+++ coopgest/routes/system.py
@@ -2,14 +2,10 @@ from datetime import datetime
 
 from flask import Blueprint, jsonify
 
-from coopgest.http_helpers import login_required
-
-
 bp = Blueprint("system", __name__)
 
 
 @bp.route("/api/health")
-@login_required
 def api_health():
     return jsonify({
         "status": "ok",
